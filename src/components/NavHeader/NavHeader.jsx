64f82823e1e81d6ed848928f3baec4e7e637a019
JinKyu <rlawlsrb167@naver.com>
2024-01-08 15:49:06 +0900

    CSS : NavHeader.jsx

--- src/components/NavHeader/NavHeader.jsx
+++ src/components/NavHeader/NavHeader.jsx
@@ -4,28 +4,30 @@ import { FaVideo } from "react-icons/fa6";
 import { RiGhostSmileFill } from "react-icons/ri";
 import { MdSportsVolleyball } from "react-icons/md";
 import { FaNewspaper } from "react-icons/fa6";
+import styles from "./NavHeader.module.css";
+
 export default function NavHeader() {
   return (
-    <header>
-      <Link to="/">
-        <h1>OverTheTop</h1>
+    <header className={styles.header}>
+      <Link to="/" className={styles.logo}>
+        <h1 className={styles.title}>OverTheTop</h1>
       </Link>
-      <nav>
-        <button>
-          <FaVideo />
-          <span>영화</span>
+      <nav className={styles.menu}>
+        <button className={styles.item}>
+          <FaVideo className={styles.icon} />
+          <span className={styles.category}>영화</span>
         </button>
-        <button>
-          <RiGhostSmileFill />
-          <span>애니메이션</span>
+        <button className={styles.item}>
+          <RiGhostSmileFill className={styles.icon} />
+          <span className={styles.category}>애니메이션</span>
         </button>
-        <button>
-          <MdSportsVolleyball />
-          <span>스포츠</span>
+        <button className={styles.item}>
+          <MdSportsVolleyball className={styles.icon} />
+          <span className={styles.category}>스포츠</span>
         </button>
-        <button>
-          <FaNewspaper />
-          <span>뉴스</span>
+        <button className={styles.item}>
+          <FaNewspaper className={styles.icon} />
+          <span className={styles.category}>뉴스</span>
         </button>
       </nav>
     </header>
