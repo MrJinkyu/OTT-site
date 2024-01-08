70119a9dee0266ad696fe58eda725f90263f6677
JinKyu <rlawlsrb167@naver.com>
2024-01-08 15:18:08 +0900

    HTML : NavHeader.jsx

--- src/components/NavHeader/NavHeader.jsx
+++ src/components/NavHeader/NavHeader.jsx
@@ -1,5 +1,33 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import { FaVideo } from "react-icons/fa6";
+import { RiGhostSmileFill } from "react-icons/ri";
+import { MdSportsVolleyball } from "react-icons/md";
+import { FaNewspaper } from "react-icons/fa6";
 export default function NavHeader() {
-  return <div>NavHeader</div>;
+  return (
+    <header>
+      <Link to="/">
+        <h1>OverTheTop</h1>
+      </Link>
+      <nav>
+        <button>
+          <FaVideo />
+          <span>영화</span>
+        </button>
+        <button>
+          <RiGhostSmileFill />
+          <span>애니메이션</span>
+        </button>
+        <button>
+          <MdSportsVolleyball />
+          <span>스포츠</span>
+        </button>
+        <button>
+          <FaNewspaper />
+          <span>뉴스</span>
+        </button>
+      </nav>
+    </header>
+  );
 }
