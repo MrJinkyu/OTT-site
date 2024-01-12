d857e3be6c1745daf566914ea8bb195d19dba991
JinKyu <rlawlsrb167@naver.com>
2024-01-13 00:44:15 +0900

    VideoDetail.jsx 비디오 상세페이지 구현

--- src/components/PlayBanner/PlayBanner.jsx
+++ src/components/PlayBanner/PlayBanner.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import styles from "./PlayBanner.module.css";
 import { useFilter } from "../../context/FilterContext";
+import { useNavigate } from "react-router-dom";
 
 export default function PlayBanner() {
   const { selectId, filter } = useFilter();
+  const navigate = useNavigate();
   return (
     <section className={styles.banner}>
       <div className={styles.info}>
         <p className={styles.title}>OTT {filter}</p>
-        {selectId && <button className={styles.playBtn}>Play</button>}
+        {selectId && (
+          <button
+            className={styles.playBtn}
+            onClick={() => navigate(`/videos/watch/${selectId}`)}
+          >
+            Play
+          </button>
+        )}
       </div>
       <div className={styles.preview}>
         {selectId && <div className={styles.hideTop}></div>}
--- src/pages/VIdeoDetail/VideoDetail.jsx
+++ src/pages/VIdeoDetail/VideoDetail.jsx
@@ -1,5 +1,29 @@
 import React from "react";
-
+import { useFilter } from "../../context/FilterContext";
+import { FaArrowLeft } from "react-icons/fa";
+import styles from "./VideoDetail.module.css";
+import { useNavigate } from "react-router-dom";
 export default function VideoDetail() {
-  return <div>VideoDetail</div>;
+  const { selectId } = useFilter();
+  const navigate = useNavigate();
+  return (
+    <section className={styles.container}>
+      <button className={styles.backBtn} onClick={() => navigate(-1)}>
+        <FaArrowLeft />
+        <span className={styles.backText}>뒤로가기</span>
+      </button>
+      <iframe
+        id="player"
+        type="text/html"
+        src={`http://www.youtube.com/embed/${selectId}`}
+        style={{
+          border: "none",
+          width: "80%",
+          height: "100%",
+        }}
+        title={selectId}
+        controls="0"
+      />
+    </section>
+  );
 }
