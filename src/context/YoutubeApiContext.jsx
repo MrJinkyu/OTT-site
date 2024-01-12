956d954474cd292b7a1eb247db2f1bd90b8b0c4f
JinKyu <rlawlsrb167@naver.com>
2024-01-13 03:20:47 +0900

    mock data 사용

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,12 +1,12 @@
 import { createContext, useContext } from "react";
 import Youtube from "../apis/youtube";
-// import FakeYoutubeClient from "../apis/fakeYoutubeClient";
-import YoutubeClient from "../apis/youtubeClient";
+import FakeYoutubeClient from "../apis/fakeYoutubeClient";
+// import YoutubeClient from "../apis/youtubeClient";
 
 const YoutubeApiContext = createContext();
 
-// const client = new FakeYoutubeClient();
-const client = new YoutubeClient();
+const client = new FakeYoutubeClient();
+// const client = new YoutubeClient();
 const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
