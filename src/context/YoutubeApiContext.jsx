c111d2591cf71d4e12bfc1fa1fe93643500a8c95
JinKyu <rlawlsrb167@naver.com>
2024-02-16 03:15:58 +0900

    mock data 사용 시 배포 에러 해결 시도

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,12 +1,14 @@
 import { createContext, useContext } from "react";
 import Youtube from "../apis/youtube.js";
-import FakeYoutubeClient from "../apis/fakeYoutubeClient.js";
+import MockYoutubeClient from "../apis/mockYoutubeClient.js";
+// import FakeYoutubeClient from "../apis/fakeYoutubeClient.js";
 // import YoutubeClient from "../apis/youtubeClient.js";
 
 const YoutubeApiContext = createContext();
 
-const client = new FakeYoutubeClient();
+// const client = new FakeYoutubeClient();
 // const client = new YoutubeClient();
+const client = new MockYoutubeClient();
 const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
